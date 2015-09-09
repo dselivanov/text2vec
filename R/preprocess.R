a7d5dcfd6779179b0ce25328a177ca3b4f42d9dd
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2015-09-09 18:16:47 +0300

    simple preprocessing utils

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,68 +1,41 @@
+#' @name simple_preprocess
+#' @title simple standart text preprocessing function.
+#' @param change_case_func - \link{function} performing case transformation -
+#' convert text between upper case, lower case. \link{tolower} by default.
+#' It should take \code{caracter} vectors and return \code{caracter} vectors.
+#' Use \link{identity} or \code{NULL} if no transformation needed.
+#' @param clean_func - \link{function} performing text cleaning.
+#' By default it only keep latin symbols '[a-z]+' and replace other symbols by
+#' whitespace. Should take \code{caracter} vectors and return \code{caracter} vectors.
+#' Use \link{identity} or \code{NULL} if no transformation needed.
+#' @param strip_space - \link{logical}, wheteher replace multiple whitespace by signle one.
+#' @param trim_space - \link{logical}, wheteher remove trailing and leading space.
+#' @return - \link{character} vectors - preprocessed text.
+#' @examples
+#' doc <- c(" first  second", "bla, bla, blaa 33  ")
+#' simple_preprocess(doc)
 #' @export
-simple_tokenizer <- function (text, what = "character", sep = ' ', quote = "", strip.white = TRUE, quiet = TRUE, ...)
-{
-  scan(text = text, what = what, sep = sep, quote = quote, strip.white = strip.white, quiet = quiet, ...)
-}
-#' @export
-regexp_tokenizer <- function (text, split = '([[:space:]]|[[:punct:]])+', ...)
-{
-  strsplit(text, split = split, ...)[[1]]
-}
-#' @export
-clean_text <- function(charVector,
-                      patternLookupPipe =c("[^[:alnum:]]+"),
-                      patternReplacementPipe = c(" "))
-{
-  if(! is.vector(patternLookupPipe, mode = 'character')) stop('patternLookupPipe should be character vector')
-  if(! is.vector(patternReplacementPipe, mode = 'character')) stop('patternReplacementPipe should be character vector')
-  if(length(patternLookupPipe) != length(patternReplacementPipe)) stop('pattern and replacement length should be equal')
-  res <- charVector
-  for (i in 1:length(patternLookupPipe))
-    res <- gsub(pattern = patternLookupPipe[i], replacement = patternReplacementPipe[i], x = res)
-  res
-}
-#' @export
-preprocess_text <- function(text,
-                            patternLookupPipe = c("[^[:alnum:]]+"),
-                            patternReplacementPipe = c(" "),
-                            letterCaseTransformation = tolower,
-                            tokenizer = simple_tokenizer,
-                            stopwords = character(0),
-                            stemming_function = NULL,
-                            ...)
-{
-  text %>%
-    iconv(., to = 'UTF-8', sub = ' ') %>% enc2native %>%
-    clean_text(patternLookupPipe = patternLookupPipe, patternReplacementPipe = patternReplacementPipe) %>%
-    (function(textVector) {
-      if (is.function(letterCaseTransformation)) {
-        letterCaseTransformation(textVector)
-      } else {
-        if(!is.null(letterCaseTransformation)) warning("letterCaseTransformation should be a FUNCTION to transform text or NULL if no transformation needed.")
-    }}) %>%
-    tokenizer %>%
-    (function(x) {
-      if(is.character(stopwords) && length(stopwords) > 0) x[!(x %in% stopwords)]
-      else x
-    }) %>%
-    (function(textVector) {
-      if (is.function(stemming_function))
-        stemming_function(textVector)
-      else textVector
-    })
-}
+simple_preprocess <- function(txt,
+                              change_case_func = tolower,
+                              clean_func = (function(x) gsub(pattern = "[^a-z]", replacement = " ", x = x)),
+                              strip_space = T,
+                              trim_space = T) {
 
-#' @export
-preprocess_corpus <- function(corpus,
-                              PreprocessFunction = PreprocessText,
-                              parallerismLevel = 1,
-                              mc.cores = ceiling(detectCores() / 2),
-                              ...)
-{
-  splits <- split_vector(corpus, parallerismLevel = parallerismLevel, splits = mc.cores)
-  unlist(mclapply(splits,
-                  function(range, inputCorpus, ...) lapply(inputCorpus[range[1]:range[2]], preprocess_text, ...),
-                  corpus,
-                  mc.cores = mc.cores),
-         ,recursive = FALSE)
+  case_func    <-   if(is.function(change_case_func)) change_case_func else identity
+
+  clean_func   <-   if(is.function(clean_func)) clean_func else identity
+
+  strip_space_func <- if(isTRUE(strip_space)) {
+    function(x) gsub(pattern = "\\s+", replacement = " ", x = x)
+  } else identity
+
+  trim_space_func <- if(isTRUE(trim_space)) {
+    function(x) gsub(pattern = "^\\s|\\s$", replacement = "", x)
+  } else identity
+
+   txt %>%
+     case_func %>%
+     clean_func %>%
+     strip_space_func %>%
+     trim_space_func
 }
