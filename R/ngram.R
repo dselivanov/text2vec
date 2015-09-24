65a6a9b9ccc4be7dbae35462407558f091b7973d
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2015-09-24 09:53:59 +0300

    clean

--- R/ngram.R
+++
@@ -1,8 +0,0 @@
-# library(microbenchmark)
-# x <- list(letters[c(1:15, 10:20)])
-# 15 times faster
-# microbenchmark::microbenchmark(
-#   r2v <- r2vec::find_ngrams(dat = x, n = 3, verbose = F)[[1]],
-#   tmlite <- tmlite::ngram(x[[1]], n_min = 1, n_max = 3, delim = ' ')
-# )
-# setdiff(tmlite, r2v)
