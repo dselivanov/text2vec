b2b735a605d697ab78030a1defbb2be406aadd28
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2015-10-29 15:28:19 +0300

    ngram generation inside corpus and vocabulary classes

--- src/Corpus.h
+++ src/Corpus.h
@@ -5,25 +5,71 @@ using namespace std;
 class Corpus {
 public:
 
-  void insert_document( const vector <string> &terms, int ngram_min, int ngram_max, const string ngram_delim);
+  void insert_terms (vector< string> &terms);
 
-  void insert_document_batch( const vector <vector <string> > &docs_batch, int ngram_min, int ngram_max, const string ngram_delim = "_");
+  void insert_document(const CharacterVector terms);
+
+  void insert_document_batch(const ListOf<const CharacterVector> docs_batch);
+
+//   virtual void insert_document(const CharacterVector doc) {
+//     vector< string > ngrams = get_ngrams(doc);
+//     insert_terms(ngrams);
+//   }
+//   virtual void insert_document_batch(const ListOf<const CharacterVector> docs_batch) {
+//     for (auto it:docs_batch)
+//       insert_document(it);
+//   }
+
 
   // total number of documents in corpus
-  int get_doc_count() { return doc_count; };
+  uint32_t get_doc_count() { return doc_count; };
 
   // total number of tokens in corpus
   uint32_t get_token_count() {return token_count;};
 
-  void ngram_generator(const CharacterVector terms,
-                       std::function<void(const string)> term_handler);
+  vector<string> get_ngrams(const CharacterVector terms) {
+    // iterates through input vector by window of size = n_max and build n-grams
+    // for terms ["a", "b", "c", "d"] and n_min = 1, n_max = 2
+    // will build 1:3-grams in following order
+    //"a"     "a_b"   "a_b_c" "b"     "b_c"   "b_c_d" "c"     "c_d"   "d"
+
+    size_t len = terms.size();
+
+    // calculate res size
+    size_t out_len = 0;
+    if(len >= this->ngram_min)
+      for(size_t i = this->ngram_min; i <= this->ngram_max; i++)
+        out_len += (len - i) + 1;
+    vector< string> res(out_len);
+
+    string k_gram;
+    size_t k, i = 0, last_observed;
+    for(size_t j = 0; j < len; j ++ ) {
+      k = 0;
+      last_observed = j + k;
+      while (k < this->ngram_max && last_observed < len) {
+        if( k == 0) {
+          k_gram = terms[last_observed];
+        }
+        else
+          k_gram = k_gram + this->ngram_delim + terms[last_observed];
+        if(k >= this->ngram_min - 1) {
+          res[i] = k_gram;
+          i++;
+        }
+        k = k + 1;
+        last_observed = j + k;
+      }
+    }
+    return res;
+  }
 
 protected:
   // token counter
   uint32_t token_count;
   size_t nnz;
   //document counter
-  int doc_count;
+  uint32_t doc_count;
 
   // ngram bounds
   uint32_t ngram_min;
--- src/Vocabulary.h
+++ src/Vocabulary.h
@@ -30,7 +30,7 @@ public:
              const string ngram_delim = "_"):
   ngram_min(ngram_min), ngram_max(ngram_max),
   ngram_delim(ngram_delim),
-  sentence_count(0), token_count(0) {};
+  document_count(0), token_count(0) {};
 
   void insert_terms (vector< string> &terms) {
     typename unordered_map < string, TermStat > :: iterator term_iterator;
@@ -50,10 +50,47 @@ public:
     }
   }
 
-  void insert_sentence(const CharacterVector terms) {
-    this->sentence_count++;
+  vector<string> get_ngrams(const CharacterVector terms) {
+    // iterates through input vector by window of size = n_max and build n-grams
+    // for terms ["a", "b", "c", "d"] and n_min = 1, n_max = 2
+    // will build 1:3-grams in following order
+    //"a"     "a_b"   "a_b_c" "b"     "b_c"   "b_c_d" "c"     "c_d"   "d"
+
+    size_t len = terms.size();
+
+    // calculate res size
+    size_t out_len = 0;
+    if(len >= this->ngram_min)
+      for(size_t i = this->ngram_min; i <= this->ngram_max; i++)
+        out_len += (len - i) + 1;
+    vector< string> res(out_len);
+
+    string k_gram;
+    size_t k, i = 0, last_observed;
+    for(size_t j = 0; j < len; j ++ ) {
+      k = 0;
+      last_observed = j + k;
+      while (k < this->ngram_max && last_observed < len) {
+        if( k == 0) {
+          k_gram = terms[last_observed];
+        }
+        else
+          k_gram = k_gram + this->ngram_delim + terms[last_observed];
+        if(k >= this->ngram_min - 1) {
+          res[i] = k_gram;
+          i++;
+        }
+        k = k + 1;
+        last_observed = j + k;
+      }
+    }
+    return res;
+  }
+
+  void insert_document(const CharacterVector terms) {
+    this->document_count++;
     this->temp_document_word_set.clear();
-    vector< string> ngrams = get_ngrams(terms, ngram_min, ngram_max, ngram_delim);
+    vector< string> ngrams = get_ngrams(terms);
     insert_terms(ngrams);
 
     typename unordered_map < string, TermStat > :: iterator term_iterator;
@@ -64,9 +101,9 @@ public:
     }
   }
 
-  void insert_sentence_batch(const ListOf<const CharacterVector> sentence_batch) {
-   for(auto s:sentence_batch)
-     insert_sentence(s);
+  void insert_document_batch(const ListOf<const CharacterVector> document_batch) {
+   for(auto s:document_batch)
+     insert_document(s);
   }
 
   List vocab_stat() {
@@ -98,7 +135,7 @@ private:
   uint32_t ngram_max;
   const string ngram_delim;
 
-  uint32_t sentence_count;
+  uint32_t document_count;
   uint32_t token_count;
   RCPP_UNORDERED_SET< string > temp_document_word_set;
 };
