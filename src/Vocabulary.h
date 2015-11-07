ac2f4d3aa7013c7e8f2c301b0948259c9b219e47
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2015-11-07 14:34:51 +0300

    DRY - keep ngram generator in single place
    Former-commit-id: b75427c64b812157f78a2d1e9e3035edaabf0611

--- src/Vocabulary.h
+++ src/Vocabulary.h
@@ -69,47 +69,10 @@ public:
     }
   }
 
-  vector<string> get_ngrams(const CharacterVector terms) {
-    // iterates through input vector by window of size = n_max and build n-grams
-    // for terms ["a", "b", "c", "d"] and n_min = 1, n_max = 2
-    // will build 1:3-grams in following order
-    //"a"     "a_b"   "a_b_c" "b"     "b_c"   "b_c_d" "c"     "c_d"   "d"
-
-    size_t len = terms.size();
-
-    // calculate res size
-    size_t out_len = 0;
-    if(len >= this->ngram_min)
-      for(size_t i = this->ngram_min; i <= this->ngram_max; i++)
-        out_len += (len - i) + 1;
-    vector< string> res(out_len);
-
-    string k_gram;
-    size_t k, i = 0, last_observed;
-    for(size_t j = 0; j < len; j ++ ) {
-      k = 0;
-      last_observed = j + k;
-      while (k < this->ngram_max && last_observed < len) {
-        if( k == 0) {
-          k_gram = terms[last_observed];
-        }
-        else
-          k_gram = k_gram + this->ngram_delim + terms[last_observed];
-        if(k >= this->ngram_min - 1) {
-          res[i] = k_gram;
-          i++;
-        }
-        k = k + 1;
-        last_observed = j + k;
-      }
-    }
-    return res;
-  }
-
   void insert_document(const CharacterVector terms) {
     this->document_count++;
     this->temp_document_word_set.clear();
-    vector< string> ngrams = get_ngrams(terms);
+    vector< string> ngrams = get_ngrams(terms, this->ngram_min, this->ngram_max, this->ngram_delim);
     insert_terms(ngrams);
 
     typename unordered_map < string, uint32_t > :: iterator term_iterator;
