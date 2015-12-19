c91c7bb1c2cdfa411afe931ba5526cd4b0dba2e7
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2015-12-19 16:28:41 +0300

    iterators tests, related to #5
    Former-commit-id: e7e954e77a6de66cf2a06945983409124e62c04c

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
 library(text2vec)
-
+data("movie_review")
 test_check("text2vec")
---
+++ tests/testthat/test-iterators.R
@@ -0,0 +1,39 @@
+context("text2vec iterators")
+
+N <- 100
+########################################
+# create temp files and dirs
+########################################
+txt_1 <- movie_review$review[1:N]
+txt_2 <- movie_review$review[(N + 1):2*N]
+
+tmp_dir <- tempdir()
+temp_file_1 <- tempfile(tmpdir = tmp_dir)
+temp_file_2 <- tempfile(tmpdir = tmp_dir)
+
+writeLines(txt_1, con = temp_file_1)
+writeLines(txt_2, con = temp_file_2)
+
+########################################
+# test
+########################################
+test_that("ifiles", {
+  it <- ifiles(c(temp_file_1, temp_file_2))
+  it2 <- itoken(it, preprocess_function = tolower, tokenizer = word_tokenizer, progessbar = F)
+  v <- vocabulary(it2)
+  expect_equal(nrow(v$vocab), 6954)
+})
+
+test_that("idir", {
+  it <- idir(path = tmp_dir)
+  it2 <- itoken(it, preprocess_function = tolower, tokenizer = word_tokenizer, progessbar = F)
+  v <- vocabulary(it2)
+  expect_equal(nrow(v$vocab), 6954)
+})
+
+test_that("ilines", {
+  it <- ilines(con = temp_file_1, n = 10)
+  it2 <- itoken(it, preprocess_function = tolower, tokenizer = word_tokenizer, progessbar = F)
+  v <- vocabulary(it2)
+  expect_equal(nrow(v$vocab), 4562)
+})
