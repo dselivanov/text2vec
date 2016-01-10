9416c0ae118fa53ff2dc3f744dd0435641bdcf19
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2016-01-10 17:58:40 +0300

    startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,14 @@ loadModule("HashCorpus", TRUE)
 loadModule("VocabularyBuilder", TRUE)
 loadModule("GloveFitter", TRUE)
 
-.onUnload <- function (libpath) {
+.onUnload <- function(libpath) {
   library.dynam.unload("text2vec", libpath)
+}
+
+.onAttach <- function(libname, pkgname) {
+  # Runs when attached to search() path such as by library() or require()
+  if (interactive()) {
+    packageStartupMessage("text2vec is under active development - API can be changed.
+                          \nFor more examples see http://dsnotes.com/categories.html#text2vec-ref")
+  }
 }
