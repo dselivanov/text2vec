7c3c9aa414f4eab7afc34dd062800a1573038986
Dmitry Selivanov <selivanov.dmitriy@gmail.com>
2016-01-28 20:32:04 +0300

    more clear single create_corpus function. with vectorizer (hash or vocab) argument.

--- R/corpus.R
+++ R/corpus.R
@@ -1,4 +1,12 @@
-#' @name create_vocab_corpus
+corpus_insert <- function(corpus, iterator) {
+  foreach(val = iterator ) %do% {
+    corpus$insert_document_batch(val)
+    attr(corpus, 'ids') <- c(attr(corpus, 'ids'), names(val))
+  }
+  corpus
+}
+
+#' @name create_corpus
 #' @title RAM-friendly streaming corpus construction.
 #' @description This functions allow to create corpus objects (vocabulary or hash based),
 #' which are stored outside of R's heap and wrapped via Reference Classes using Rcpp-Modules.
@@ -8,71 +16,105 @@
 #' objects/files which are orders of magnitude larger that available RAM.
 #' @param iterator iterator over \code{list} of \code{character} vectors.
 #' Each element is a list of tokens = tokenized and normalized strings.
-#' @param vocabulary \code{text2vec_vocabulary} object, see \link{vocabulary}.
-#' @param grow_dtm \code{logical} should we grow Document-Term matrix
-#' during corpus construction or not.
-#' @param skip_grams_window \code{integer} window for Term-Cooccurence matrix
-#' construction. 0L points to do not construct such matrix.
+#' @param vectorizer \code{function} vectorizer function.
 #' @return corpus object,
 #' We can add documents into this corpus by reference - no copy at all.
 #' See source code for details.
 #' For full process example see \link{get_dtm}.
+#' @seealso \link{vectorizers}
 #' @export
-create_vocab_corpus <- function(iterator,
-                                vocabulary,
-                                grow_dtm = TRUE,
-                                skip_grams_window = 0L) {
+create_corpus <- function(iterator,
+                          vectorizer) {
+  if (!inherits(iterator, 'iter'))
+    stop("iterator argument should be iterator over list of tokens (class 'iter')")
+
+  corpus <- vectorizer(iterator)
+  corpus
+}
+
+#' @name vectorizers
+#' @title creates vocabulary or hash based vectorizer.
+#' @description This function creates text vectorizer function
+#' which used in corpus construction.
+#' @param grow_dtm \code{logical} should we grow Document-Term matrix
+#' during corpus construction or not.
+#' @param skip_grams_window \code{integer} window for Term-Cooccurence matrix
+#' construction. 0L points to do not construct such matrix.
+#' @return vectorizer \code{function}
+#' @seealso \link{create_corpus}
+#' @examples
+#' data("movie_review")
+#' N <- 100
+#' vectorizer <- hash_vectorizer(2 ^ 18, c(1L, 2L))
+#' it <- itoken(movie_review$review[1:N], preprocess_function = tolower,
+#'              tokenizer = word_tokenizer, chunks_number = 10)
+#' corpus <- create_corpus(it, vectorizer)
+#' hash_dtm <- get_dtm(corpus)
+#'
+#' it <- itoken(movie_review$review[1:N], preprocess_function = tolower,
+#'              tokenizer = word_tokenizer, chunks_number = 10)
+#' v <- vocabulary(it, c(1L, 1L) )
+#'
+#' vectorizer <- vocab_vectorizer(v)
+#'
+#' it <- itoken(movie_review$review[1:N], preprocess_function = tolower,
+#'              tokenizer = word_tokenizer, chunks_number = 10)
+#'
+#' corpus <- create_corpus(it, vectorizer)
+#' voacb_dtm <- get_dtm(corpus)
+
+
+#' @rdname vectorizers
+#' @param vocabulary \code{text2vec_vocabulary} object, see \link{vocabulary}.
+#' @export
+vocab_vectorizer <- function(vocabulary,
+                             grow_dtm = TRUE,
+                             skip_grams_window = 0L) {
+
   if (!grow_dtm && skip_grams_window == 0L)
     stop("At least one of the arguments 'grow_dtm', 'skip_grams_window' should
          satisfy grow_dtm == TRUE or skip_grams_window > 0")
-
-  if (!inherits(iterator, 'iter'))
-    stop("iterator argument should be iterator over list of tokens (class 'iter')")
 
   vocab_corpus <- new(VocabCorpus,
                       vocab = vocabulary$vocab$terms,
                       ngram_min = vocabulary$ngram[["ngram_min"]],
                       ngram_max = vocabulary$ngram[["ngram_max"]],
                       window_size = skip_grams_window)
-  attr(vocab_corpus, 'ids') <- character(0)
-  corpus_insert(vocab_corpus, iterator)
-}
 
-#' @rdname create_vocab_corpus
-#' @param feature_hasher \code{text2vec_feature_hasher} object, which contains meta information
-#' about feature hashing. See \link{feature_hasher} for details.
-#' @seealso \link{feature_hasher}.
-#' @export
-create_hash_corpus <- function(iterator,
-                               feature_hasher = feature_hasher(),
-                               grow_dtm = TRUE,
-                               skip_grams_window = 0) {
-
-  if (!inherits(iterator, 'iter'))
-    stop("iterator argument should be iterator over list of tokens (class 'iter')")
-
-  hash_corpus <- new(HashCorpus,
-                     hash_size = feature_hasher$hash_size,
-                     ngram_min = feature_hasher$ngram[["ngram_min"]],
-                     ngram_max = feature_hasher$ngram[["ngram_max"]],
-                     window_size = skip_grams_window,
-                     feature_hasher$signed_hash)
-  attr(hash_corpus, 'ids') <- character(0)
-  corpus_insert(hash_corpus, iterator)
-}
-
-corpus_insert <- function(corpus, iterator) {
-  while (TRUE) {
-    val <- try(nextElem(iterator), silent = T)
-    if (class(val) == "try-error") {
-      if (attributes(val)$condition$message == "StopIteration")
-        break
-      # handle other errors
-      else
-        stop(attributes(val)$condition$message)
-    }
-    corpus$insert_document_batch(val)
-    attr(corpus, 'ids') <- c(attr(corpus, 'ids'), names(val))
+  vectorizer <- function(iterator) {
+    attr(vocab_corpus, 'ids') <- character(0)
+    corpus_insert(vocab_corpus, iterator)
   }
-  corpus
+  vectorizer
+}
+
+#' @rdname vectorizers
+#' @param hash_size \code{integer} > 0 - number of hash-buckets
+#' for hashing trick (feature hashing). Preferably power of 2 number.
+#' @param ngram \code{integer} vector. The lower and upper boundary of the range of
+#' n-values for different n-grams to be extracted. All values of n such that
+#' @param signed_hash \code{logical},  indicating whether to use second hash-function
+#' to reduce impact of collisions.
+#' @export
+hash_vectorizer <- function(hash_size = 2 ^ 18,
+                            ngram = c(1L, 1L),
+                            signed_hash = FALSE,
+                            grow_dtm = TRUE,
+                            skip_grams_window = 0L) {
+
+  if (!grow_dtm && skip_grams_window == 0L)
+    stop("At least one of the arguments 'grow_dtm', 'skip_grams_window' should
+         satisfy grow_dtm == TRUE or skip_grams_window > 0")
+
+  vectorizer <- function(iterator) {
+    hash_corpus <- new(HashCorpus,
+                       hash_size = hash_size,
+                       ngram_min = ngram[[1]],
+                       ngram_max = ngram[[2]],
+                       window_size = 0,
+                       signed_hash)
+    attr(hash_corpus, 'ids') <- character(0)
+    corpus_insert(hash_corpus, iterator)
+  }
+  vectorizer
 }
---
+++ R/dtm.R
@@ -0,0 +1,92 @@
+#' @name get_dtm
+#' @title Creates Document-Term matrix
+#' @description Creates Document-Term matrix from Corpus object.
+#' @param corpus HashCorpus or VocabCorpus object.
+#' See \link{create_corpus} for details.
+#' @param type character, one of \code{c("dgCMatrix", "dgTMatrix", "lda_c")}.
+#' "lda_c" - Blei's lda-c format (list of 2*doc_terms_size),
+#' see \url{https://www.cs.princeton.edu/~blei/lda-c/readme.txt}
+#' @examples
+#' \dontrun{
+#' N <- 1000
+#' it <- itoken(movie_review$review[1:N], preprocess_function = tolower,
+#'              tokenizer = word_tokenizer, chunks_number = 10)
+#' v <- vocabulary(it, c(1L, 1L) )
+#'
+#' #remove very common and uncommon words
+#' pruned_vocab = prune_vocabulary(v, term_count_min = 10,
+#'  doc_proportion_max = 0.8, doc_proportion_min = 0.001,
+#'  max_number_of_terms = 10000)
+#'
+#' vectorizer <- vocab_vectorizer(v)
+#' it <- itoken(movie_review$review[1:N], preprocess_function = tolower,
+#'              tokenizer = word_tokenizer, chunks_number = 10)
+#' corpus <- create_corpus(it, vectorizer)
+#' dtm <- get_dtm(corpus)
+#'
+#' tf_scale_matrix <- get_tf(dtm, type = 'tf')
+#' dtm_tf <- tf_scale_matrix %*% dtm
+#' dtm_tf_idf <- get_tf %*% m %*% get_idf(dtm)
+#'
+#' # The same result we can obtain using tfidf_transformer function
+#' dtm_tf_idf_2 <- tfidf_transformer(dtm)
+#' identical(dtm_tf_idf, dtm_tf_idf_2)
+#' }
+#' @export
+get_dtm <- function(corpus, type = c("dgCMatrix", "dgTMatrix", "lda_c")) {
+  if (inherits(corpus, 'Rcpp_VocabCorpus') || inherits(corpus, 'Rcpp_HashCorpus')) {
+    type <- match.arg(type)
+    dtm <- corpus$get_dtm()
+    dtm@Dimnames[[1]] <- attr(corpus, 'ids')
+    switch(type,
+           dgTMatrix = dtm,
+           dgCMatrix = as(dtm, "dgCMatrix"),
+           lda_c = to_lda_c(dtm),
+           NULL)
+  }
+  else
+    stop("corpus should be instance of Rcpp_HashCorpus or Rcpp_VocabCorpus classes")
+
+}
+
+#' @name create_dtm
+#' @title Document-Term Matrix construction.
+#' @description High-level function for Document-Term Matrix construction.
+#' If parallel backend is registered, it will construct DTM in multiple threads.
+#' @details User should keep in mind, that he/she should split data itself and
+#' provide list of \link{itoken} iterators. Each element of \code{itoken_list}
+#' will be handled at separate thread and at the end they will be combined.
+#' @param itoken_list \code{list} of iterators over tokens - \code{itoken}.
+#' Each element is a list of tokens = tokenized and normalized strings.
+#' @param vectorizer \code{function} vectorizer function.
+#' @param type character, one of \code{c("dgCMatrix", "dgTMatrix", "lda_c")}.
+#' "lda_c" - Blei's lda-c format (list of 2*doc_terms_size),
+#' see \url{https://www.cs.princeton.edu/~blei/lda-c/readme.txt}
+#' @param ... - arguments to \link{foreach} function which is used to iterate
+#' over \code{itoken_list} under the hood.
+#' @return Document-Term Matrix
+#' @seealso \link{itoken}
+#' @export
+create_dtm <- function(itoken_list,
+                       vectorizer,
+                       type = c("dgCMatrix", "dgTMatrix", "lda_c"), ...) {
+
+  type <- match.arg(type)
+  combine_fun <- switch(type,
+                        dgCMatrix = rbind,
+                        dgTMatrix = rbind,
+                        lda_c = c)
+
+  dtm <-
+    foreach(it = itoken_list,
+          .combine = combine_fun,
+          .inorder = T,
+          .multicombine = T,
+          ...) %dopar%
+          {
+            corp <- vectorizer(it)
+            dtm_chunk <- get_dtm(corp, 'dgTMatrix')
+            dtm_chunk
+          }
+  dtm
+}
