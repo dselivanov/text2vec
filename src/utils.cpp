5ff2ac69436b36ce34b34a60ac769eee735fb542
Dmitry Selivanov <selivanov.dmitriy@gmail.com>
2016-02-18 20:37:35 +0300

    export ngrams
    Former-commit-id: 3e89053f4c7d22c6b69fad8780b8b206e174f12e

--- src/utils.cpp
+++ src/utils.cpp
@@ -88,10 +88,80 @@ vector<string> get_ngrams(const CharacterVector terms,
   return res;
 }
 
-// //' @export
-// // [[Rcpp::export]]
-// CharacterVector get_ngrams_R(const CharacterVector terms,
-//                              uint32_t ngram_min,
-//                              uint32_t ngram_max) {
-//   return wrap(get_ngrams(terms, ngram_min, ngram_max, "_"));
+//' @export
+//' @name ngrams
+//' @title Generates ngrams
+//' @param terms input tokens
+//' @param ngram_min min number of tokens in ngram
+//' @param ngram_max max number of tokens in ngram
+//' @param sep string separator between tokems
+// [[Rcpp::export]]
+CharacterVector ngrams(const CharacterVector terms,
+                       uint32_t ngram_min,
+                       uint32_t ngram_max,
+                       const String sep = "_") {
+  return wrap(get_ngrams(terms, ngram_min, ngram_max, sep));
+}
+
+// // for unordered_map < <uint32_t, uint32_t>, T >
+// namespace std {
+// template <>
+// struct hash<std::pair<uint32_t, uint32_t>>
+// {
+//   inline uint64_t operator()(const std::pair<uint32_t, uint32_t>& k) const
+//   {
+//     //should produce no collisions
+//     //http://stackoverflow.com/a/24693169/1069256
+//     //return f << (CHAR_BIT * sizeof(size_t) / 2) | s;
+//     //http://stackoverflow.com/questions/2768890/how-to-combine-two-32-bit-integers-into-one-64-bit-integer?lq=1
+//     return (uint64_t) k.first << 32 | k.second;
+//   }
+// };
+// }
+//
+// // list of dgTMatrix as input
+// S4 add_triplet_matrix_list(ListOf<S4> lst) {
+//   unordered_map<pair<uint32_t, uint32_t>, float> res;
+//   S4 m = lst[0];
+//   IntegerVector dims = m.slot("Dim");
+//   List dimnames = m.slot("Dimnames");
+//   size_t i;
+//
+//   IntegerVector I, J;
+//   NumericVector X;
+//   for(S4 it: lst) {
+//     I = it.slot("i");
+//     J = it.slot("j");
+//     X = it.slot("x");
+//     for( i = 0; i < I.size(); i++ ) {
+//       res[make_pair(I[i], J[i])] += X[i];
+//     }
+//   }
+//
+//   size_t NNZ = res.size();
+//
+//   // result triplet sparse matrix
+//   S4 triplet_matrix("dgTMatrix");
+//
+//   // index vectors
+//   IntegerVector I_RES(NNZ), J_RES(NNZ);
+//   // value vector
+//   NumericVector X_RES(NNZ);
+//
+//   i = 0;
+//   for(auto it : res) {
+//     I_RES[i] = it.first.first;
+//     J_RES[i] = it.first.second;
+//     X_RES[i] = it.second;
+//     i++;
+//   }
+//   // construct matrix
+//   triplet_matrix.slot("i") = I_RES;
+//   triplet_matrix.slot("j") = J_RES;
+//   triplet_matrix.slot("x") = X_RES;
+//   // set dimensions
+//   triplet_matrix.slot("Dim") = dims;
+//   // set dimension names
+//   triplet_matrix.slot("Dimnames") = dimnames;
+//   return triplet_matrix;
 // }
