f7e095edc264f63854ad7071f9608b14c0032892
Dmitry Selivanov <selivanov.dmitriy@gmail.com>
2016-02-20 12:34:53 +0300

    adjust tests
    Former-commit-id: fbc6a88766bdf1050290f45544b4178d921c09e5

--- tests/testthat/test-tcm.R
+++ tests/testthat/test-tcm.R
@@ -1,10 +1,5 @@
 context("tcm construction")
 
-split_into <- function(vec, nparts) {
-  max_part_len <- ceiling(length(vec) / nparts)
-  suppressWarnings( split(vec, rep(1:nparts, each = max_part_len)) )
-}
-
 train_ind <- 1:1000
 N_WORKER <- 4
 
@@ -16,7 +11,7 @@ txt_splits <- split_into(txt, N_WORKER)
 
 test_that("tcm with foreach", {
   TERMS_LIMIT = 3000L
-  jobs <- lapply(txt_splits, itoken, tolower, word_tokenizer)
+  jobs <- lapply(txt_splits, itoken, tolower, word_tokenizer, chunks_number = 1)
 
   v <- vocabulary(jobs, c(1L, 1L) ) %>%
     prune_vocabulary(term_count_min = 5, doc_proportion_max = 0.5, max_number_of_terms = TERMS_LIMIT)
@@ -26,7 +21,7 @@ test_that("tcm with foreach", {
   jobs <- lapply(txt_splits, itoken, tolower, word_tokenizer)
 
   tcm <- create_tcm(jobs, vectorizer)
-  expect_equal(tcm['from', 'like'],  10)
+  expect_true(tcm['his', 'he'] - 42 < 1e-5)
   expect_equal(dim(tcm),  c(TERMS_LIMIT, TERMS_LIMIT))
   expect_true(isTriangular(tcm, upper = TRUE))
 })
