3e786b9c1e4f0ebe1b4e8e5f61e3798b91ab07aa
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-03-24 10:47:26 -0400

    Change analogue to analogy wherever it appears

--- R/glove.R
+++ R/glove.R
@@ -61,11 +61,9 @@
 #'               max_cost = 100, convergence_threshold = 0.005)
 #' word_vectors <- fit$word_vectors[[1]] + fit$word_vectors[[2]]
 #' rownames(word_vectors) <- rownames(tcm)
-#' qlst <- prepare_analogue_questions('./questions-words.txt', rownames(word_vectors))
-#' res <- check_analogue_accuracy(questions_lst = qlst, m_word_vectors = word_vectors)
+#' qlst <- prepare_analogy_questions('./questions-words.txt', rownames(word_vectors))
+#' res <- check_analogy_accuracy(questions_lst = qlst, m_word_vectors = word_vectors)
 #' }
-
-
 glove <- function(tcm,
                   vocabulary_size,
                   word_vectors_size,
