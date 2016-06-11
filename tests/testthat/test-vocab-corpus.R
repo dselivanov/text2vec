e46dd91c3656e0b3c67411e79964999055459838
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2016-06-11 19:55:29 +0300

    do not test utf-8 on cran

--- tests/testthat/test-vocab-corpus.R
+++ tests/testthat/test-vocab-corpus.R
@@ -11,16 +11,6 @@ get_test_iterator <- function()
          preprocess_function = tolower,
          tokenizer = word_tokenizer,
          progessbar = F)
-test_that("Vocabulary with UTF-8 strings", {
-  all_text = list(enc2utf8(c("标准间", "太差", "房间", "还", "不如", "星", "的",
-                             "而且", "设施", "非常", "陈旧", "建议", "酒店", "把", "老", "的",
-                             "标准间", "从", "新", "改善")))
-  it = itoken(all_text,ids = 1)
-  vocab = create_vocabulary(it)
-  expect_true(all(Encoding(vocab$vocab$terms) == 'UTF-8'))
-  expect_equal(setdiff(unique(all_text[[1]]), vocab$vocab$terms), character(0))
-  expect_equal(setdiff(vocab$vocab$terms, unique(all_text[[1]])), character(0))
-})
 
 test_that("Vocabulary pruning", {
   iterator <- get_test_iterator()
