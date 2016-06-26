5b37021e958d0526ac78799439310d6995c55ac8
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2016-06-26 19:10:50 +0300

    bunch of distance measures, including relaxed word mover distance - related to #92.

---
+++ R/distance.R
@@ -0,0 +1,121 @@
+# work on 0,1 valued sparse matrices
+jaccard_dist <- function(x, y = NULL, format = 'dgCMatrix') {
+  if (!inherits(x, 'sparseMatrix'))
+    stop("at the moment jaccard distance defined only for sparse matrices")
+  # union x
+  rs_x = rowSums(x)
+  if (is.null(y)) {
+    # intersect x
+    RESULT = tcrossprod(x)
+    rs_y = rs_x
+  } else {
+    if (!inherits(y, 'sparseMatrix'))
+      stop("at the moment jaccard distance defined only for sparse matrices")
+    # intersect x y
+    RESULT = tcrossprod(x, y)
+    # union y
+    rs_y = rowSums(y)
+  }
+  RESULT = as(RESULT, 'dgTMatrix')
+  # add 1 because of zero-based indices in sparse matrices
+  RESULT@x <- RESULT@x / (rs_x[RESULT@i + 1L] + rs_y[RESULT@j + 1L] - RESULT@x)
+  if (!inherits(RESULT, format))
+    RESULT = as(RESULT, format)
+  RESULT
+}
+
+#' @name distances
+#' @param x first matrix.
+#' @param y second matrix. \code{y = NULL} is taken to be the same matrix as \code{x}.
+#' @param method \code{character} or instance of \code{tet2vec_distance} class.
+#' The distance measure to be used. One of \code{c("cosine", "euclidean", "jaccard")} or \link{RWMD}.
+#' \code{RWMD} works only on bag-of-words matrices.
+#' Also user should keep in mind, that distance = 1 - similarity.
+#' @param norm \code{character} - how to scale input matrices.
+#' @param verbose \code{logical} whether to display additional information during calculations
+#' @title Pairwise Distance Matrix Computation
+#' @description \code{dist2} calculates pairwise distances between the
+#' rows of two data matrices. \bold{Note} that some methods work only on sparse matrices and
+#' others work only on dense matrices.
+#' @details Computes the distance matrix computed by using the specified method.
+#' Similar to \link{dist} function, but works with two matrices.
+#' @return \code{dist2} returns \code{matrix} of distances between each row of
+#' matrix \code{x} and each row of matrix \code{y}.
+#' @export
+dist2 <- function(x, y = NULL, method = c('cosine', 'euclidean', 'jaccard'),
+                     norm = c('none', 'l1', 'l2'), verbose = TRUE) {
+  stopifnot(inherits(x, "matrix") || inherits(x, "sparseMatrix"))
+  stopifnot(inherits(method, "text2vec_distance") || inherits(method, "character"))
+
+  if (!is.null(y)) {
+    FLAG_TWO_MATRICES_INPUT = TRUE
+  }
+  if (FLAG_TWO_MATRICES_INPUT) {
+    stopifnot(inherits(y, "matrix") || inherits(y, "sparseMatrix"))
+    stopifnot(ncol(x) == ncol(y))
+    stopifnot(colnames(x) == colnames(y))
+  }
+
+  norm = match.arg(norm)
+  RESULT = NULL
+  if (inherits(method, "character")) {
+    if (method == 'cosine') {
+      if (norm == 'l1') stop("l2 norm should be used with 'cosine' method")
+      if (FLAG_TWO_MATRICES_INPUT) y = normalize(y, norm)
+      x = normalize(x, norm)
+      RESULT = 1 - tcrossprod(x, y)
+    }
+    if (method == 'euclidean') {
+      if (!inherits(x, 'matrix') || !inherits(y, 'matrix'))
+        stop("At the moment eucludian distance can be calculated only for
+              dense matrices of class 'matrix'")
+      # transpose, because euclidean_dist() function calculates dist between columns
+      x = t(normalize(x, norm))
+      if (FLAG_TWO_MATRICES_INPUT) {
+        y = t(normalize(y, norm))
+        RESULT = euclidean_dist(x, y)
+      } else
+        RESULT = euclidean_dist(x, x)
+    }
+    if (method == 'jaccard') {
+      if (!inherits(x, 'sparseMatrix'))
+        stop("at the moment jaccard distance defined only for sparse matrices")
+
+      if (norm != 'none')
+        warning("No normalization is needed - values will be convertet to 0, 1 aumatically! \\
+                'jaccard' can be computed only on sets which should be encoded as sparse matrices of 0, 1.")
+
+      x@x = sign(x@x)
+
+      if (FLAG_TWO_MATRICES_INPUT) {
+        y@x = sign(y@x)
+      }
+      RESULT = 1 - jaccard_dist(x, y)
+    }
+  }
+  if (inherits(method, "text2vec_distance")) {
+    if (inherits(method, "RWMD")) {
+      if (norm != 'none')
+        warning("RWMD can be computed only on bag-of-words matrices - raw word-counts.
+                Usually no normalization is needed - l1 normalization will be done aumatically!")
+      RESULT = method$distance(x, y,  verbose)
+    }
+  }
+  if (is.null(RESULT))
+    stop(paste("not implemented for class", class(method)))
+  RESULT
+}
+#
+# #' @rdname distances
+# #' @title "Parallel" Distance Matrix Computation
+# #' @description \code{pdist2} calculates "parallel" distances between the rows of two data matrices.
+# #' @details \code{pdist2} takes two matrices and return a single vector.
+# #' giving the ‘parallel’ distances of the vectors.
+# #' @return \code{pdist2} returns \code{vector} of "parallel" distances between rows
+# #' of \code{x} and \code{y}.
+# #' @export
+# pdist2 <- function(x, y = x, method = 'cosine', norm = c('none' ,'l1', 'l2')) {
+#   stop("not implemented yet")
+#   NULL
+# }
+#
---
+++ tests/testthat/test-distances.R
@@ -0,0 +1,62 @@
+context("distances")
+ind <- 1:100
+m1 <- matrix(1:15, nrow = 5)
+m2 <- matrix(1:12, nrow = 4)
+
+tokens <- movie_review$review[ind] %>% tolower %>% word_tokenizer
+v = create_vocabulary(itoken(tokens, progessbar = F)) %>% prune_vocabulary(term_count_min = 3)
+corpus = create_corpus(itoken(tokens, ids = movie_review$id[ind], progessbar = F),
+                       vectorizer = vocab_vectorizer(v, skip_grams_window = 5))
+dtm = get_dtm(corpus)
+tcm = get_dtm(corpus)
+i1 = 1:10
+i2 = 1:20
+
+test_that("cosine", {
+  expect_error(dist2(dtm[i1, ], dtm[i2, ], method = 'cosine', norm = 'l1'))
+
+  cos_dist = dist2(dtm[i1, ], dtm[i2, ], method = 'cosine', norm = 'l2')
+  expect_equal(nrow(cos_dist), length(i1))
+  expect_equal(ncol(cos_dist), length(i2))
+  expect_true(cos_dist[1, 1] < 1e-10)
+  expect_true(cos_dist[1, 2] - 0.4294539 < 1e-5 )
+  expect_lte(max(cos_dist), 1)
+})
+
+test_that("jaccard", {
+  expect_error(dist2(m1, m2, method = 'jaccard', norm = 'l2'))
+  jac_dist = dist2(dtm[i1, ], dtm[i2, ], method = 'jaccard', norm = 'none')
+  expect_equal(nrow(jac_dist), length(i1))
+  expect_equal(ncol(jac_dist), length(i2))
+  expect_true(jac_dist[1, 1] < 1e-10)
+  expect_true(jac_dist[1, 2] - 0.8207547 < 1e-5 )
+  expect_lte(max(jac_dist), 1)
+  m1 <- matrix(1, nrow = 5, ncol = 4)
+  m2 <- matrix(1, nrow = 3, ncol = 4)
+})
+
+test_that("euclidean", {
+  # euclidean works only with dense matrices
+  expect_error(dist2(dtm[i1, ], dtm[i2, ], method = 'euclidean'))
+
+  euc_dist = dist2(m1, m2, method = 'euclidean')
+  expect_equal(nrow(euc_dist), nrow(m1))
+  expect_equal(ncol(euc_dist), nrow(m2))
+  # calculate distances with base dist function
+  base_dist = dist(rbind(m1, m2), method = 'euclidean')
+  # subset correct elements - they should correspond to two input matrices
+  base_dist = as.matrix(base_dist)[1:nrow(m1), nrow(m1) + 1:nrow(m2)]
+  dimnames(base_dist) <- NULL
+  expect_equal(base_dist, euc_dist)
+})
+
+test_that("relaxed word mover distance", {
+  glove_model <- GloVe(word_vectors_size = 50, vocabulary = v, x_max = 10)
+  wv = fit_predict(glove_model, tcm, n_iter = 10, verbose = F)
+  rwmd_model = RWMD(word_vectors = wv)
+  rwmd_dist <- dist2(dtm[i1, ], dtm[i2, ], method = rwmd_model, norm = 'none')
+  expect_equal(nrow(rwmd_dist), length(i1))
+  expect_equal(ncol(rwmd_dist), length(i2))
+  expect_lte(rwmd_dist[1,1], 1e-10)
+  # expect_lte(rwmd_dist[2,1] - 3.564279e-01, 1e-5)
+})
