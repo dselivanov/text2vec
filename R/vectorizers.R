7762769dcb22d81ea737b294d9f29a15e1fd4328
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2017-04-04 19:51:01 +0400

    force lazy evaluation to capture vocabulary for case of PSOCK cluster computing

--- R/vectorizers.R
+++ R/vectorizers.R
@@ -83,6 +83,7 @@ corpus_insert = function(corpus_ptr, iterator, grow_dtm, skip_grams_window_conte
 #' @param vocabulary \code{text2vec_vocabulary} object, see \link{create_vocabulary}.
 #' @export
 vocab_vectorizer = function(vocabulary) {
+  force(vocabulary)
   vectorizer = function(iterator, grow_dtm, skip_grams_window_context, window_size, weights) {
     vocab_corpus_ptr = cpp_vocabulary_corpus_create(vocabulary$vocab$terms,
                                                     vocabulary$ngram[["ngram_min"]],
